7cecf76375ceb6fdacc3d0ed6b4995cd68f28154
mthompsonc <m.paz.thompson@gmail.com>
2017-09-26 10:37:36 -0300

    adding exercise

---
+++ repaso.js
@@ -0,0 +1,45 @@
+/* calcular sueldo semanal de un trabajador con base en las horas que trabaja
+Ej: Carol gana 10.000 x hora y trabaja 20 horas a la semana.
+ 200000
+*/
+
+//Dos formas de hacer el ejercicio
+/*
+
+
+function salaryWeek(hours, payment){
+var salary = hours * payment;
+return salary;
+}
+
+function salaryWeek(hours, payment){
+return hours * payment;
+}
+
+
+---------------
+
+Calcular la cantidad de digitos de un numero.
+por ejemplo: 1986 deberá retornar 4
+
+
+function numberOfDigits(number){
+  var counter = 0;
+  var digits = number.toString();
+  for (var i = 0; i < digits.length; i++){
+      counter ++;
+  }
+  return counter;
+}
+
+-------------------------
+
+Palíndrome
+*/
+
+function palindrome(word){
+  var long= word.length;
+  for(var i = 0; i < word.length; i++){
+    if(word[i] !== wo
+  }
+}
